32bbd9ad3bd80acb33fb8ae0ecba88fd5660c5ea
vadsimus <vadsimus@gmail.com>
2020-09-11 08:40:46 +0300

    Django add template

--- django-sites/testsite/mysite/news/views.py
+++ django-sites/testsite/mysite/news/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world')
+    news = News.objects.all()
+    context = {
+        'news': news, 'title': 'список новостей'
+    }
+    return render(request, 'news/index.html', context)
+
 
 def test(request):
     return HttpResponse('<h1>тестовая страница</h1>')
-
-
