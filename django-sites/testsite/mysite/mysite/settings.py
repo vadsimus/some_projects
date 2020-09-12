21a417af6b866576ab1297d3a1cf4ba792187217
vadsimus <vadsimus@gmail.com>
2020-09-12 12:04:05 +0300

    Django static

--- django-sites/testsite/mysite/mysite/settings.py
+++ django-sites/testsite/mysite/mysite/settings.py
@@ -113,6 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'mysite/static'
+]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
--- django-sites/testsite/mysite/news/models.py
+++ django-sites/testsite/mysite/news/models.py
@@ -11,6 +11,9 @@ class News(models.Model):
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
+
     def __str__(self):
         return self.title
 
