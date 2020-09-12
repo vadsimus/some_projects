a2c2d8973461d9d1288e039fea75f6e47a96aae9
vadsimus <vadsimus@gmail.com>
2020-09-12 14:13:01 +0300

    Django forms

---
+++ django-sites/testsite/mysite/news/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Category
+
+
+class NewsForm(forms.Form):
+    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={'class': "form-control"}))
+    content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(
+        attrs={
+            'class': "form-control",
+            'rows': 5
+        }
+    ))
+    is_published = forms.BooleanField(label='опубликовано?', initial=True)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Выберите категорию', widget=forms.Select(attrs={'class': "form-control"}))
--- django-sites/testsite/mysite/news/urls.py
+++ django-sites/testsite/mysite/news/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', index, name='home'),
     path('test/', test),
     path('category/<int:category_id>', get_category, name='category'),
-    path('news/<int:news_id>', view_news, name='view_news')
+    path('news/<int:news_id>', view_news, name='view_news'),
+    path('news/add_news', add_news, name='add_news')
 ]
