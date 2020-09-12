caa9c42794cc82eca798f7f9168e140df944de64
vadsimus <vadsimus@gmail.com>
2020-09-12 09:59:51 +0300

    Django

--- django-sites/testsite/mysite/news/urls.py
+++ django-sites/testsite/mysite/news/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('test/', test)
+    path('', index, name='home'),
+    path('test/', test),
+    path('category/<int:category_id>', get_category, name='category')
 ]
--- django-sites/testsite/mysite/news/views.py
+++ django-sites/testsite/mysite/news/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 # Create your views here.
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
-        'news': news, 'title': 'список новостей'
+        'news': news,
+        'title': 'список новостей',
+        'categories': categories
     }
     return render(request, 'news/index.html', context)
 
 
 def test(request):
     return HttpResponse('<h1>тестовая страница</h1>')
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
