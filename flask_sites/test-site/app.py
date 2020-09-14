e80af825088c1b9c426091a6068b7f3c4640fc18
vadsimus <vadsimus@gmail.com>
2020-09-14 09:10:24 +0300

    Flask #4 view article

--- flask_sites/test-site/app.py
+++ flask_sites/test-site/app.py
@@ -30,6 +30,18 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+
+
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
@@ -42,7 +54,7 @@ def create_article():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return 'при добавлениее статьи произошла ошибка'
     else:
